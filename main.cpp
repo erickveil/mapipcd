a0056e68c1a40ad2a20a9bd7b00ce38159eadd10
eveil <eveil@comtechcom.net>
2021-03-02 08:59:01 -0800

    Main defined
    Main process is pretty simple. Just declare the class and call start.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 // Copyright 2021 Erick Veil
 
+#include "ipchub.h"
+
 #include <QCoreApplication>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    IpcHub hub;
+    hub.start();
 
     return a.exec();
 }
