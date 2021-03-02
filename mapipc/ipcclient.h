f4b43e6f8ee40fff66218c1f4de96e0d4210cafa
eveil <eveil@comtechcom.net>
2021-03-02 09:22:28 -0800

    Added socket client and started base class

---
+++ mapipc/ipcclient.h
@@ -0,0 +1,22 @@
+/**
+ * ipcclient.h
+ * Erick Veil
+ * 2021-03-02
+ * Copyright 2021 Erick Veil
+ */
+#ifndef IPCCLIENT_H
+#define IPCCLIENT_H
+
+
+/**
+ * @brief The IpcClient class
+ * Initializes and utilizes local socket clients to communicate with mapipcd.
+ * Provides an interface for getting and setting values.
+ */
+class IpcClient
+{
+public:
+    IpcClient();
+};
+
+#endif // IPCCLIENT_H
